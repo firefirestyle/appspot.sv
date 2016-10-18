9d3ada1e0b8be9753d9623c0ae34b3edcf7c5246
kyorohiro <kyorohiro@gmail.com>
2016-10-18 11:33:49 +0900

    ..

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 
 	//	"errors"
 
-	"github.com/firefirestyle/gominioauth/twitter"
+	"github.com/firefirestyle/go.miniblob"
+	"github.com/firefirestyle/go.minioauth/twitter"
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
 )
@@ -14,6 +15,7 @@ import (
 const (
 	UrlApiRoot = "/api/v1"
 )
+
 const (
 	UrlTwitterTokenUrlRedirect                          = "twitter/tokenurl/redirect"
 	UrlTwitterTokenUrlRedirect_callbackUrl              = "cb"
@@ -23,7 +25,27 @@ const (
 	UrlTwitterTokenCallback_callbackUrl                 = "cb"
 )
 
+const (
+	UrlBlobRequestUrl = "blob/requesturl"
+	UrlBlobCallback   = "blob/callback"
+)
+
 var twitterHandlerObj *twitter.TwitterHandler = nil
+var blobHandlerObj *miniblob.BlobHandler = nil
+
+func GetBlobHandlerObj(ctx context.Context) *miniblob.BlobHandler {
+	if blobHandlerObj == nil {
+		blobHandlerObj = miniblob.NewBlobHandler(
+			UrlApiRoot+"/"+UrlBlobCallback, "secret sign", //
+			miniblob.BlobManagerConfig{
+				ProjectId:   "firefirestyle",
+				Kind:        "blobstore",
+				CallbackUrl: UrlBlobCallback,
+			},
+			miniblob.BlobHandlerOnEvent{})
+	}
+	return blobHandlerObj
+}
 
 func GetTwitterHandlerObj(ctx context.Context) *twitter.TwitterHandler {
 	if twitterHandlerObj == nil {
@@ -57,4 +79,13 @@ func initApi() {
 		//	w.Header().Add("Access-Control-Allow-Origin", "*")
 		GetTwitterHandlerObj(appengine.NewContext(r)).TwitterLoginExit(w, r)
 	})
+	http.HandleFunc(UrlApiRoot+"/"+UrlBlobRequestUrl, func(w http.ResponseWriter, r *http.Request) {
+		//	w.Header().Add("Access-Control-Allow-Origin", "*")
+		GetBlobHandlerObj(appengine.NewContext(r)).BlobRequestToken(w, r)
+	})
+	http.HandleFunc(UrlApiRoot+"/"+UrlBlobCallback, func(w http.ResponseWriter, r *http.Request) {
+		//	w.Header().Add("Access-Control-Allow-Origin", "*")
+		GetBlobHandlerObj(appengine.NewContext(r)).HandleUploaded(w, r)
+	})
+
 }
