b0557fcb1b413644cdaf86435df28b73a8cdaa34
kyorohiro <kyorohiro@gmail.com>
2016-10-30 15:54:32 +0900

    ..

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 	//	"errors"
 
 	arthundler "github.com/firefirestyle/go.miniarticle/hundler"
-	miniblob "github.com/firefirestyle/go.miniblob/blob"
-	blobhandler "github.com/firefirestyle/go.miniblob/handler"
+	//	miniblob "github.com/firefirestyle/go.miniblob/blob"
+	//	blobhandler "github.com/firefirestyle/go.miniblob/handler"
 	"github.com/firefirestyle/go.minioauth/twitter"
 	"github.com/firefirestyle/go.miniprop"
 	"github.com/firefirestyle/go.minisession"
 	//	"github.com/firefirestyle/go.miniuser"
-	userhundler "github.com/firefirestyle/go.miniuser/hundler"
+	userhundler "github.com/firefirestyle/go.miniuser/handler"
 	//
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
@@ -22,9 +22,7 @@ import (
 	//"crypto/rand"
 	//"encoding/binary"
 	//"strconv"
-	"errors"
 	"io/ioutil"
-	"strings"
 
 	//	"google.golang.org/appengine/blobstore"
 )
@@ -40,23 +38,27 @@ const (
 	UrlTwitterTokenUrlRedirect = "/api/v1/twitter/tokenurl/redirect"
 	UrlTwitterTokenCallback    = "/api/v1/twitter/tokenurl/callback"
 
-	UrlBlobRequestUrl     = "/api/v1/blob/requesturl"
-	UrlBlobCallback       = "/api/v1/blob/callback"
-	UrlBlobGet            = "/api/v1/blob/get"
-	UrlUserGet            = "/api/v1/user/get"
-	UrlUserFind           = "/api/v1/user/find"
-	UrlMeLogout           = "/api/v1/me/logout"
-	UrlArtNew             = "/api/v1/art/new"
-	UrlArtUpdate          = "/api/v1/art/update"
-	UrlArtFind            = "/api/v1/art/find"
-	UrlArtGet             = "/api/v1/art/get"
-	UrlArtBlobGet         = "/api/v1/art/getblob"
-	UrlArtRequestBlobUrl  = "/api/v1/art/requestbloburl"
-	UrlArtCallbackBlobUrl = "/api/v1/art/callbackbloburl"
+	UrlBlobRequestUrl      = "/api/v1/blob/requesturl"
+	UrlBlobCallback        = "/api/v1/blob/callback"
+	UrlBlobGet             = "/api/v1/blob/get"
+	UrlUserGet             = "/api/v1/user/get"
+	UrlUserFind            = "/api/v1/user/find"
+	UrlUserBlobGet         = "/api/v1/user/getblob"
+	UrlUserRequestBlobUrl  = "/api/v1/user/requestbloburl"
+	UrlUserCallbackBlobUrl = "/api/v1/user/callbackbloburl"
+	UrlMeLogout            = "/api/v1/me/logout"
+	UrlArtNew              = "/api/v1/art/new"
+	UrlArtUpdate           = "/api/v1/art/update"
+	UrlArtFind             = "/api/v1/art/find"
+	UrlArtGet              = "/api/v1/art/get"
+	UrlArtBlobGet          = "/api/v1/art/getblob"
+	UrlArtRequestBlobUrl   = "/api/v1/art/requestbloburl"
+	UrlArtCallbackBlobUrl  = "/api/v1/art/callbackbloburl"
 )
 
 var twitterHandlerObj *twitter.TwitterHandler = nil
-var blobHandlerObj *blobhandler.BlobHandler = nil
+
+//var blobHandlerObj *blobhandler.BlobHandler = nil
 var sessionMgrObj *minisession.SessionManager = nil
 var userHandlerObj *userhundler.UserHandler = nil
 var artHandlerObj *arthundler.ArticleHandler = nil
@@ -79,73 +81,15 @@ func GetArtHundlerObj(ctx context.Context) *arthundler.ArticleHandler {
 
 func GetUserHundlerObj(ctx context.Context) *userhundler.UserHandler {
 	if userHandlerObj == nil {
-		userHandlerObj = userhundler.NewUserHandler(userhundler.UserHandlerManagerConfig{
-			ProjectId:   "firefirestyle",
-			UserKind:    "user",
-			RelayIdKind: "relayId",
-			SessionKind: "session",
-		}, userhundler.UserHandlerOnEvent{})
+		userHandlerObj = userhundler.NewUserHandler(UrlUserCallbackBlobUrl,
+			userhundler.UserHandlerManagerConfig{ //
+				ProjectId:   "firefirestyle",
+				UserKind:    "user",
+				RelayIdKind: "relayId",
+				SessionKind: "session",
+			}, userhundler.UserHandlerOnEvent{})
 	}
 	return userHandlerObj
-}
-
-func GetBlobHandlerObj(ctx context.Context) *blobhandler.BlobHandler {
-	if blobHandlerObj == nil {
-		blobHandlerObj = blobhandler.NewBlobHandler(
-			UrlBlobCallback, appengine.VersionID(ctx), //
-			miniblob.BlobManagerConfig{
-				ProjectId:   "firefirestyle",
-				Kind:        "blobstore",
-				PointerKind: "blobstorepointer",
-				CallbackUrl: UrlBlobCallback,
-			},
-			blobhandler.BlobHandlerOnEvent{
-				OnBlobRequest: func(w http.ResponseWriter, r *http.Request, outputProp *miniprop.MiniProp, blobHandlerObj *blobhandler.BlobHandler) (string, map[string]string, error) {
-					//
-					// login check
-					bodyBytes, _ := ioutil.ReadAll(r.Body)
-					propObj := miniprop.NewMiniPropFromJson(bodyBytes)
-					token := propObj.GetString("token", "")
-					ctx := appengine.NewContext(r)
-
-					loginCheckInfo := GetUserHundlerObj(ctx).GetSessionMgr().CheckLoginId(ctx, token, minisession.MakeAccessTokenConfigFromRequest(r))
-					if loginCheckInfo.IsLogin == false {
-						return "", nil, errors.New("failed to wrong token : (1)")
-					}
-					//
-					// path check
-					dir := r.URL.Query().Get("dir")
-					if true == strings.HasPrefix(dir, "/user") {
-						Debug(ctx, ">>>>>> AAA <<<<<<"+loginCheckInfo.AccessTokenObj.GetUserName())
-						if false == strings.HasPrefix(dir, "/user/"+loginCheckInfo.AccessTokenObj.GetUserName()) {
-							return "", nil, errors.New("failed to wrong token : (2)")
-						}
-					} else {
-						return "", nil, errors.New("failed to wrong token : (3)")
-					}
-					return loginCheckInfo.AccessTokenObj.GetLoginId(), map[string]string{}, nil
-				},
-				OnBlobComplete: func(w http.ResponseWriter, r *http.Request, outputProp *miniprop.MiniProp, blobHandlerObj *blobhandler.BlobHandler, blobObj *miniblob.BlobItem) error {
-					dir := r.URL.Query().Get("dir")
-					if true == strings.HasPrefix(dir, "/user") {
-						ctx := appengine.NewContext(r)
-						userName := strings.Replace(dir, "/user/", "", -1)
-						userMgrObj := GetUserHundlerObj(ctx)
-						userObj, userErr := userMgrObj.GetUserFromRelayId(ctx, userName)
-						if userErr != nil {
-							outputProp.SetString("error", "not found user")
-							return userErr
-						}
-						userObj.SetIconUrl("key://" + blobObj.GetBlobKey())
-						GetUserHundlerObj(ctx).SaveUserWithImmutable(ctx, userObj)
-						return nil
-					} else {
-						return errors.New("unsupport")
-					}
-				},
-			})
-	}
-	return blobHandlerObj
 }
 
 func GetTwitterHandlerObj(ctx context.Context) *twitter.TwitterHandler {
@@ -182,21 +126,6 @@ func initApi() {
 		w.Header().Add("Access-Control-Allow-Origin", "*")
 		GetTwitterHandlerObj(appengine.NewContext(r)).TwitterLoginExit(w, r)
 	})
-	// blob
-	http.HandleFunc(UrlBlobRequestUrl, func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		GetBlobHandlerObj(appengine.NewContext(r)).HandleBlobRequestToken(w, r)
-	})
-
-	http.HandleFunc(UrlBlobCallback, func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		GetBlobHandlerObj(appengine.NewContext(r)).HandleUploaded(w, r)
-	})
-
-	http.HandleFunc(UrlBlobGet, func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		GetBlobHandlerObj(appengine.NewContext(r)).HandleGet(w, r)
-	})
 
 	// user
 	http.HandleFunc(UrlUserGet, func(w http.ResponseWriter, r *http.Request) {
@@ -206,6 +135,18 @@ func initApi() {
 	http.HandleFunc(UrlUserFind, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Origin", "*")
 		GetUserHundlerObj(appengine.NewContext(r)).HandleFind(w, r)
+	})
+	http.HandleFunc(UrlUserRequestBlobUrl, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		GetUserHundlerObj(appengine.NewContext(r)).HandleBlobRequestToken(w, r)
+	})
+	http.HandleFunc(UrlUserCallbackBlobUrl, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		GetUserHundlerObj(appengine.NewContext(r)).HandleBlobUpdated(w, r)
+	})
+	http.HandleFunc(UrlUserBlobGet, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		GetUserHundlerObj(appengine.NewContext(r)).HandleBlobGet(w, r)
 	})
 
 	// me
